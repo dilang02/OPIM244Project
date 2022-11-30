fab0a1280b02db73b5b5bf33b6aea7145351a552
dilang02 <dng18@georgetown.edu>
2022-11-30 16:30:24 -0500

    Web App

--- web_app/routes/DSS_routes.py
+++ web_app/routes/DSS_routes.py
@@ -3,30 +3,24 @@
 
 from flask import Blueprint, request, render_template, redirect, flash
 
-from app.FINANCE_DSS import tool_1, tool_2, tool_3
+#from app.FINANCE_DSS import tool_1, tool_2, tool_3
 
 DSS_routes = Blueprint("DSS_routes", __name__)
 
 
-@DSS_routes.route("/DSS/dashboard")
+@DSS_routes.route("/dss/dashboard")
 def DSS_dashboard():
     print("DECISION SUPPORT SYSTEM...")
 
     try:
-        from getpass import getpass
-        API_KEY = getpass("Please input your AlphaVantage API Key: ")
+
 
 
 
         #flash("Fetched Latest Unemployment Data!", "success")
         return render_template("DSS.html",
-            while True:
-                if tool_number == "1": # Call requested functionality based on user input
-                    tool_1()
-                elif tool_number == "2":
-                    tool_2()
-                elif tool_number == "3":
-                    tool_3() 
+        data = [1,2,3]
+
         )
     except Exception as err:
         print('OOPS', err)
