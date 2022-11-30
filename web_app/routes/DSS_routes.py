33580076121039fab5b6dba1c19f1b67c111b418
dilang02 <dng18@georgetown.edu>
2022-11-30 11:58:17 -0500

    Update DSS_routes.py

--- web_app/routes/DSS_routes.py
+++ web_app/routes/DSS_routes.py
@@ -3,26 +3,30 @@
 
 from flask import Blueprint, request, render_template, redirect, flash
 
-from app.unemployment import fetch_unemployment_data, format_pct
+from app.FINANCE_DSS import tool_1, tool_2, tool_3
 
-unemployment_routes = Blueprint("unemployment_routes", __name__)
+DSS_routes = Blueprint("DSS_routes", __name__)
 
 
-@unemployment_routes.route("/unemployment/dashboard")
-def unemployment_dashboard():
-    print("UNEMPLOYMENT DASHBOARD...")
+@DSS_routes.route("/DSS/dashboard")
+def DSS_dashboard():
+    print("DECISION SUPPORT SYSTEM...")
 
     try:
-        data = fetch_unemployment_data()
-        latest = data[0]
-        latest_rate_pct = format_pct(float(latest["value"]))
-        latest_date = latest["date"]
+        from getpass import getpass
+        API_KEY = getpass("Please input your AlphaVantage API Key: ")
+
+
 
         #flash("Fetched Latest Unemployment Data!", "success")
-        return render_template("unemployment_dashboard.html",
-            latest_rate_pct=latest_rate_pct,
-            latest_date=latest_date,
-            data=data
+        return render_template("DSS.html",
+            while True:
+                if tool_number == "1": # Call requested functionality based on user input
+                    tool_1()
+                elif tool_number == "2":
+                    tool_2()
+                elif tool_number == "3":
+                    tool_3() 
         )
     except Exception as err:
         print('OOPS', err)
@@ -34,13 +38,13 @@ def unemployment_dashboard():
 # API ROUTES
 #
 
-@unemployment_routes.route("/api/unemployment.json")
-def unemployment_api():
-    print("UNEMPLOYMENT DATA (API)...")
-
-    try:
-        data = fetch_unemployment_data()
-        return data
-    except Exception as err:
-        print('OOPS', err)
-        return {"message":"Unemployment Data Error. Please try again."}, 404
+#@DSS_routes.route("/api/unemployment.json")
+#def unemployment_api():
+ #   print("UNEMPLOYMENT DATA (API)...")
+#
+ #   try:
+  #      data = fetch_unemployment_data()
+   #     return data
+    #except Exception as err:
+     #   print('OOPS', err)
+      #  return {"message":"Unemployment Data Error. Please try again."}, 404
